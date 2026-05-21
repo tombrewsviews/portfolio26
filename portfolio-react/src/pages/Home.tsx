cf0e8f9f276bb0952b725f4b301ea5d217645a6b
Tom Brews Views <parandykt@gmail.com>
2026-05-22 00:37:56 +0800

    feat: assemble Home page

--- portfolio-react/src/pages/Home.tsx
+++ portfolio-react/src/pages/Home.tsx
@@ -1,3 +1,25 @@
+import Nav from '../components/Nav';
+import Hero from '../components/Hero';
+import CreativeBanner from '../components/CreativeBanner';
+import About from '../components/About';
+import WorkList from '../components/WorkList';
+import ExperimentsList from '../components/ExperimentsList';
+import WhatShapedMe from '../components/WhatShapedMe';
+import Footer from '../components/Footer';
+
 export default function Home() {
-  return <main className="container">Home</main>;
+  return (
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <CreativeBanner />
+        <About />
+        <WorkList />
+        <ExperimentsList />
+        <WhatShapedMe />
+      </main>
+      <Footer />
+    </>
+  );
 }
