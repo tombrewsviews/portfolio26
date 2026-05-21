fdc7269445b0ca23953058f5693e63937126b2a0
Tom Brews Views <parandykt@gmail.com>
2026-05-22 00:38:58 +0800

    feat: assemble CaseStudy page with 404 on unknown slug

--- portfolio-react/src/pages/CaseStudy.tsx
+++ portfolio-react/src/pages/CaseStudy.tsx
@@ -1,6 +1,32 @@
 import { useParams } from 'react-router-dom';
+import { getProject } from '../content/lookup';
+import NotFound from './NotFound';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import CaseHero from '../components/case/CaseHero';
+import VideoIntro from '../components/case/VideoIntro';
+import Overview from '../components/case/Overview';
+import MetricsGrid from '../components/case/MetricsGrid';
+import RoleBlock from '../components/case/RoleBlock';
+import Challenges from '../components/case/Challenges';
 
 export default function CaseStudy() {
   const { slug } = useParams();
-  return <main className="container">Case study: {slug}</main>;
+  const project = getProject(slug ?? '');
+  if (!project) return <NotFound />;
+
+  return (
+    <>
+      <Nav />
+      <main>
+        <CaseHero project={project} />
+        <VideoIntro project={project} />
+        <Overview project={project} />
+        <MetricsGrid project={project} />
+        <RoleBlock project={project} />
+        <Challenges project={project} />
+      </main>
+      <Footer />
+    </>
+  );
 }
